e8731ebb4dcd8c0d04669e716fdda4d11caae978
aakashDeveloper <ahanda205@gmail.com>
2018-12-04 02:25:22 +0100

    a

---
+++ formsapp/src/app/customerForms/customerForms.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-forms',
+    templateUrl: './customerForms.component.html'
+})
+
+export class CustomerFormComponent {
+    languages: any[] = ['NodeJs', 'ReactJs', 'AngularJS', 'GO'];
+}
